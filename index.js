8cf90fa090bd4a6c965ea1e9fbad4af881391a0d
Nihal Konda <nihal.konda@sjsu.edu>
2020-04-01 09:23:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+function sendMail(to,subject,html){
+    fetch('http://06a1cf11.ngrok.io/mail', {
+        method: 'post',
+        body: JSON.stringify({to,subject,html})
+      })
+    .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.text();
+    })
+    .then(function(response) {
+        console.log('mail sent successfully');
+    });
+}
+
+module.exports={
+    sendMail
+}
